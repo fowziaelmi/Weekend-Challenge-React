151c8ab9f2a61996dc7e695dd03c58f6692605a6
Faduma Elmi <fowziiae@gmail.com>
2021-02-20 22:24:33 -0600

    added axios GET route

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,32 @@
 import React from 'react';
 import './App.css';
-
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 function App() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
-        </header>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
-      </div>
-    );
+  useEffect(() => {
+    fetchData();
+  }, []); //end useEffect
+  const fetchData = () => {
+    axios
+      .get('/gallery')
+      .then((response) => {
+        console.log('get response:', response.data);
+      })
+      .catch((error) => {
+        alert('Error fetching data.');
+        console.log('GET error:', error);
+      });
+  }; // end fetchData
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Gallery of My Life</h1>
+      </header>
+      <p>Gallery goes here</p>
+      <img src="images/goat_small.jpg" />
+    </div>
+  );
 }
 
 export default App;
