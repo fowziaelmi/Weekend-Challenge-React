2985d8f045e559d2dedc5eacb69c0b6aa4efedce
Faduma Elmi <fowziiae@gmail.com>
2021-02-20 22:09:14 -0600

    added photos to gallery

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,28 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+  {
+    id: 1,
+    path: 'images/goat_small.jpg',
+    description: 'Photo of a goat taken at Glacier National Park.',
+    likes: 0,
+  },
+  {
+    id: 2,
+    path: 'images/kenya.jpg',
+    description: 'Photo of Nairobi National Park.',
+    likes: 0,
+  },
+  {
+    id: 3,
+    path: 'images/nephews.jpg',
+    description: 'Photo of my nephews.',
+    likes: 0,
+  },
+  {
+    id: 4,
+    path: 'images/wings.jpg',
+    description: 'Photo of yummy wings.',
+    likes: 0,
+  },
 ];
 
 module.exports = galleryItems;
