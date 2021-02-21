c31c2755f7dfc9fcdbaa1bf4f3ba72008cd6b698
Faduma Elmi <fowziiae@gmail.com>
2021-02-21 13:24:10 -0600

    added  handle to swap pic to description

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,8 +1,22 @@
+import { useState } from 'react';
+
 function GalleryItem({ pic, addLikes }) {
+  const [onClick, setOnClick] = useState(0);
+  const onClickHandle = () => {
+    if (onClick % 2 === 0) {
+      return <img src={pic.path} className="resizing" />;
+    } else {
+      return <p> {pic.description} </p>;
+    }
+  };
+
   return (
-    <div className="imageStore" key={pic.id}>
-      <img className="resizing" src={pic.path} />
-      <div>{pic.description}</div>
+    <div
+      className="imageStore"
+      key={pic.id}
+      onClick={() => setOnClick(onClick + 1)}
+    >
+      {onClickHandle()};
       <button onClick={addLikes} id={pic.id}>
         LIKE
       </button>
